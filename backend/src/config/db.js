b5c0a315c9da8884a6cb38f8eaa85fdb21e17c6d
Guutch <simonh1611@outlook.com>
2025-04-07 23:05:19 +0100

    Update Sequelize config to use SSL for Heroku Postgres

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,14 @@ console.log("DB_USER:", process.env.DB_USER);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    logging: false
-});
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+  
 
 module.exports = sequelize;
