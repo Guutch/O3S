8475f5cd591662f9f2f3303ea58b106c853535e8
Guutch <simonh1611@outlook.com>
2025-04-07 23:08:56 +0100

    Switch to DATABASE_URL with SSL for Heroku Postgres

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,9 +4,9 @@ const { Sequelize } = require('sequelize');
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_USER:", process.env.DB_USER);
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    protocol: 'postgres',
     logging: false,
     dialectOptions: {
       ssl: {
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
       }
     }
   });
+  
   
 
 module.exports = sequelize;
